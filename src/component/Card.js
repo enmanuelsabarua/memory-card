55ad1d757ec3ca8d783467fa934015481580fc4d
Enmanuel Sanchez Abarua <enmanuel.sanchez.a@gmail.com>
2023-05-19 17:06:11 -0400

    Add the shuffle functionality

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,8 +1,8 @@
 
-function Card({ characterName }) {
+function Card({ characterName, shuffle, charactersNames }) {
     return (
         <div>
-            <div className="img">
+            <div className="img" onClick={() => shuffle(charactersNames)}>
                 <img src={`../imgs/${characterName}.jpg`} alt="Anime Character" />
             </div>
 
--- src/component/Gameboard.js
+++ src/component/Gameboard.js
@@ -1,13 +1,36 @@
+import { useState } from "react";
 import images from "../imgs";
 import Card from "./Card";
 import uniqid from 'uniqid';
 
 
 function Gameboard() {
+    const [nameArr, setNameArr] = useState(images);
+
+    // Snippet from stack overflow 
+    function shuffle(array) {
+        let modifiedArr = [...array]
+        let currentIndex = modifiedArr.length, randomIndex;
+        
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+        
+            // Pick a remaining element.
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+        
+            // And swap it with the current element.
+            [modifiedArr[currentIndex], modifiedArr[randomIndex]] = [
+            modifiedArr[randomIndex], modifiedArr[currentIndex]];
+        }
+        
+        setNameArr(modifiedArr);
+    }
+
     return (
         <>
             <main className="Gameboard">
-                {images.map(characterName => <Card characterName={characterName} key={uniqid()} />)}
+                {nameArr.map(characterName => <Card shuffle={shuffle} charactersNames={nameArr} characterName={characterName} key={uniqid()} />)}
             </main>
         </>
     );
