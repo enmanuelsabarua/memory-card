1b282ec627b0eacd3f2c1215418d598c37efc1ce
Enmanuel Sanchez Abarua <enmanuel.sanchez.a@gmail.com>
2023-05-19 22:07:34 -0400

    Fix images bug

--- src/component/Card.js
+++ src/component/Card.js
@@ -4,7 +4,7 @@ function Card({ characterName, shuffle, charactersNames }) {
     return (
         <div className='card'>
             <div className="img">
-                <img src={`../imgs/${characterName}.jpg`} alt="Anime Character" id={characterName} onClick={(e) => shuffle(e, charactersNames)}/>
+                <img src={`./imgs/${characterName}.jpg`} alt="Anime Character" id={characterName} onClick={(e) => shuffle(e, charactersNames)}/>
             </div>
 
             <h2>{characterName.replace('-', ' ')}</h2>
